ce07f0f6603de25b9e30e021d8656ddb62f7cb3a
GuroKung <greenblood11@hotmail.com>
2017-05-18 04:43:10 +0700

    taskLists component

--- src/components/mainPage.js
+++ src/components/mainPage.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
-import { View, Text, TextInput, Button } from 'react-native';
+import { View, Text } from 'react-native';
 
 import moment from 'moment';
 import firebase from '../firebaseService';
-import GreenButton from './greenBtn';
+import TaskLists from './taskLists';
 
 class MainPage extends Component {
     static navigationOptions = {
@@ -13,54 +13,12 @@ class MainPage extends Component {
     constructor(props){
         super(props);
         let currentDate = moment().format("MMM Do YY");
-        let curentUser = firebase.auth().currentUser;
-        let tasks = [{ label: 'A' }, { label: 'B' }, { label: 'C' }];       
+        let curentUser = firebase.auth().currentUser;   
 
-        firebase.database().ref('users/' + curentUser.uid + '/tasks').once('value')
-        .then((snapshot) => {
-            if(!snapshot) this.setState({ tasks: [] });
-            else this.setState({ tasks: snapshot.val() });
-        })
-        .catch((error) => {
-            console.log('ERROR: ' + error.message);
-            // throw error;
-        });
-
-        this.state = { currentDate, curentUser, tasks };
-    }
-
-    async createNewTask() {
-        if (this.state.newTask) {
-            try {
-                let tasks = this.state.tasks;
-
-                if(!tasks) tasks = [];
-
-                tasks.push({
-                    label: this.state.newTask
-                });
-
-                await firebase.database().ref('users/' + this.state.curentUser.uid + '/tasks').set(tasks);
-                this.setState({ tasks });
-            } catch (error) {
-                console.log(error.toString());
-            }
-        }
-    }
-
-    clearTask(){
-
+        this.state = { currentDate, curentUser, taskUrl: 'users/' + curentUser.uid + '/tasks' };
     }
 
     render() {
-
-        let tasks = [];
-        console.log();
-        if (this.state.tasks) {
-             tasks = this.state.tasks.map((task, index) => {
-                return <Text key={index}>{task.label}</Text>
-            });
-        }
        
         return (
             <View style={{ flex: 1, flexDirection: 'column', justifyContent: 'flex-start' }}>
@@ -70,24 +28,7 @@ class MainPage extends Component {
                 <View style={{ flexDirection: 'row', justifyContent: 'center', marginTop: -15 }}>
                     <Text>{this.state.currentDate.toString()}</Text>
                 </View>
-                <View style={{flexDirection: 'column', padding: 50, justifyContent: 'center' }}>
-                    {tasks}
-                </View>
-                <View style={{ padding: 50, justifyContent: 'flex-start' }}>
-                    <TextInput
-                        style={{ height: 80 }}
-                        placeholder="Create New Task"
-                        onChangeText={(newTask) => this.setState({ newTask })}
-                    />
-                    <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'center'  }}>
-                        <GreenButton title="Add" press={() => this.createNewTask()} />
-                        <Button
-                            title="Clear"
-                            color="#000000"
-                            accessibilityLabel="Clear"
-                            onPress={this.clearTask} />
-                    </View>
-                </View>
+                <TaskLists taskUrl={this.state.taskUrl}/>
             </View>
         );
     }
--- src/components/taskLists.js
+++ src/components/taskLists.js
@@ -1,11 +1,85 @@
+import React, { Component } from 'react';
+import { View, Text, TextInput, Button } from 'react-native';
+
+import firebase from '../firebaseService';
+
+import GreenButton from './greenBtn';
+
 class TaskLists extends Component {
     constructor(props){
         super(props);
-        this.state = {  };
+
+        let tasks = [{label: 'Now Loading..'}];
+
+        firebase.database().ref(props.taskUrl).once('value')
+            .then((snapshot) => {
+                if (!snapshot) this.setState({ tasks: [] });
+                else this.setState({ tasks: snapshot.val() });
+            })
+            .catch((error) => {
+                console.log('ERROR: ' + error.message);
+                // throw error;
+            });
+
+        this.state = { tasks };
     }
+
+    async createNewTask() {
+        if (this.state.newTask) {
+            try {
+                let tasks = this.state.tasks;
+
+                if(!tasks) tasks = [];
+
+                tasks.push({
+                    label: this.state.newTask
+                });
+
+                await firebase.database().ref(this.props.taskUrl).set(tasks);
+                this.setState({ tasks });
+            } catch (error) {
+                console.log(error.toString());
+            }
+        }
+    }
+
+    clearTask() {
+        this.setState({ newTask: '' });
+    }
+
     render() {
+        let tasks = [];
+
+        if (this.state.tasks) {
+            tasks = this.state.tasks.map((task, index) => {
+                return (
+                    <View key={index} style={{ borderStyle: 'solid'}}>
+                        <Text>{task.label}</Text>
+                    </View>
+                )
+            });
+        }
         return (
-            
+            <View style={{ flex: 1, flexDirection: 'column', justifyContent: 'flex-start' }}>
+                <View style={{ flexDirection: 'column', padding: 50, justifyContent: 'center' }}>
+                    {tasks}
+                </View>
+                <View style={{ padding: 50, justifyContent: 'flex-start' }}>
+                    <TextInput
+                        style={{ height: 80 }}
+                        placeholder="Create New Task"
+                        onChangeText={(newTask) => this.setState({ newTask })}
+                    />
+                    <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'center' }}>
+                        <GreenButton title="Add" press={() => this.createNewTask()} />
+                        <Button
+                            title="Clear"
+                            color="#000000"
+                            accessibilityLabel="Clear"
+                            onPress={this.clearTask} />
+                    </View>
+                </View>
+            </View>            
         );
     }
 }
